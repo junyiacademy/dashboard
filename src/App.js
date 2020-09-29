a692edf8f0f0aa0c4e7e649c59b0647286bc7112
wadetsai <wadetsai@gmail.com>
2020-09-29 16:51:12 +0800

    '0929'

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
-import ButtonBase from '@material-ui/core/ButtonBase';
-import figData from './data.json';
+import figData from './user_count_by_week.json';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    margin: 50,
   },
   paper: {
     padding: theme.spacing(2),
     margin: 'auto',
-    maxWidth: 500,
+    maxWidth: 1200,
   },
   image: {
     width: 128,
@@ -32,16 +32,30 @@ function App() {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <Paper className={classes.paper}>
-        <Grid container spacing={2}>
-            <Grid item>
+      <Typography variant="h5" paragraph={true}>
+        均一教育平台網站公開數據
+      </Typography>
+      <Typography variant="h5" paragraph={true}>
+        前一周數據
+      </Typography>
+
+      <Grid container spacing={2}>
+          <Grid item>
+            <Paper className={classes.paper}>
               <Plot
-                data={figData['RegisterdUser']}
-                  layout={ {width: 320, height: 240, title: '各類使用者註冊人數'} }
+                data={[
+                  {
+                    x: Object.values(figData['week']),
+                    y: Object.values(figData['unique_user_cnt']),
+                    type: 'scatter',
+                    mode: 'lines',
+                  },
+                ]}
+                  layout={ {width: 1100, height: 400, title: '週活躍人數'} }
                 />
-            </Grid>
+            </Paper>
           </Grid>
-        </Paper>
+        </Grid>
       </div>
   );
 }
