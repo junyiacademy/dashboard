b75212c4df3767271b4e31978bce4f51c1b664b5
wadetsai <wadetsai@junyiacademy.org>
2020-10-05 09:24:57 +0800

    recharts

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import Plot from 'react-plotly.js';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
-import ButtonBase from '@material-ui/core/ButtonBase';
-import figData from './data.json';
+import userCountByWeek from './user_count_by_week.json';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    margin: 50,
+    marginLeft: 50,
   },
   paper: {
     padding: theme.spacing(2),
     margin: 'auto',
-    maxWidth: 500,
+    maxWidth: 1200,
   },
   image: {
     width: 128,
@@ -32,17 +33,90 @@ function App() {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <Paper className={classes.paper}>
-        <Grid container spacing={2}>
-            <Grid item>
-              <Plot
-                data={figData['RegisterdUser']}
-                  layout={ {width: 320, height: 240, title: '各類使用者註冊人數'} }
-                />
-            </Grid>
+      <Typography variant="h5" paragraph={true}>
+        均一教育平台網站公開數據
+      </Typography>
+      <Typography variant="h6" paragraph={true}>
+        上週數據
+      </Typography>
+      <Grid container spacing={2}>
+        <Grid item xs>
+          <Paper className={classes.paper}>
+            <LineChart width={1100} height={400} data={userCountByWeek}>
+              <Line type="monotone" dataKey="unique_user_cnt" stroke="#8884d8" />
+              <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+              <XAxis dataKey="week" />
+              <YAxis />
+              <Tooltip />
+            </LineChart>
+          </Paper>
+        </Grid>
+      </Grid>
+      <div >&zwj;</div>
+      <Typography variant="h6" paragraph={true}>
+        歷史數據
+      </Typography>
+      <Grid container spacing={2}>
+          <Grid item xs>
+            <Paper className={classes.paper}>
+
+            </Paper>
           </Grid>
-        </Paper>
-      </div>
+          <Grid item xs>
+            <Paper className={classes.paper}>
+
+            </Paper>
+          </Grid>
+        </Grid>
+
+      <Grid container spacing={2}>
+        <Grid item xs>
+          <Paper className={classes.paper}>
+          </Paper>
+        </Grid>
+        <Grid item xs>
+          <Paper className={classes.paper}>
+          </Paper>
+        </Grid>
+        <Grid item xs>
+          <Paper className={classes.paper}>
+          </Paper>
+        </Grid>
+      </Grid>
+      <Grid container spacing={2}>
+        <Grid item xs>
+          <Paper className={classes.paper}>
+
+          </Paper>
+        </Grid>
+      </Grid>
+      <Grid container spacing={2}>
+        <Grid item xs>
+          <Paper className={classes.paper}>
+
+          </Paper>
+        </Grid>
+      </Grid>
+      <Grid container spacing={2}>
+        <Grid item xs>
+          <Paper className={classes.paper}>
+
+          </Paper>
+        </Grid>
+      </Grid>
+      <Grid container spacing={2}>
+        <Grid item xs>
+          <Paper className={classes.paper}>
+
+          </Paper>
+        </Grid>
+        <Grid item xs>
+          <Paper className={classes.paper}>
+
+          </Paper>
+        </Grid>
+      </Grid>
+    </div>
   );
 }
 
