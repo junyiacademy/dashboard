8079775dc9f42623fe38d8526893336f9d6854c0
wadetsai <wadetsai@gmail.com>
2020-10-08 17:10:45 +0800

    1008

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,10 @@ import WAUWeekByWeek from './WAU_week_by_week.json';
 import lastWeekWAUByCity from './last_week_WAU_by_city.json';
 import regUserCategory from './reg_user_category.json';
 import contentType from './content_type.json';
+import lastWeekWAUByHour from './last_week_WAU_by_hour.json';
+import contentUsageByMonth from './content_usage_by_month.json';
+import regUserByMonth from './reg_user_by_month.json';
+import regUserByCity from './reg_user_by_city.json';
 
 const totalRegUserCnt = regUserCategory.reduce((total: number, obj: any) => total + obj.count, 0);
 const RADIAN = Math.PI / 180;
@@ -84,19 +88,55 @@ function App() {
       </Typography>
 
       <Grid container spacing={4}>
+        <Grid item xs={6}>
+          <Typography variant="h6">
+            上週24小時各時段平均活躍人數
+          </Typography>
+          <Paper className={classes.paper}>
+            <ResponsiveContainer width="100%" height={250}>
+              <LineChart data={lastWeekWAUByHour}>
+                <Line name="平均活躍人數" type="monotone" dataKey="avg_user_cnt" stroke={colors20[0]} dot={true}/>
+                <CartesianGrid stroke="#ccc" strokeDasharray="3 3" vertical={false}/>
+                <XAxis dataKey="hour"/>
+                <YAxis />
+                <Tooltip />
+              </LineChart>
+            </ResponsiveContainer>
+          </Paper>
+        </Grid>
+
+        <Grid item xs={6}>
+          <Typography variant="h6">
+            上週24小時各時段平均使用時間(分鐘)
+          </Typography>
+          <Paper className={classes.paper}>
+            <ResponsiveContainer width="100%" height={250}>
+              <LineChart data={lastWeekWAUByHour}>
+                <Line name="平均使用時間(分鐘)" type="monotone" dataKey="avg_time_minute" stroke={colors20[2]} dot={true}/>
+                <CartesianGrid stroke="#ccc" strokeDasharray="3 3" vertical={false}/>
+                <XAxis dataKey="hour"/>
+                <YAxis />
+                <Tooltip />
+              </LineChart>
+            </ResponsiveContainer>
+          </Paper>
+        </Grid>
+      </Grid>
+
+      <Grid container spacing={4}>
         <Grid item xs>
           <Typography variant="h6">
             上週各縣市活躍人數
           </Typography>
           <Paper className={classes.paper}>
-            <ResponsiveContainer width="100%" height={300}>
+            <ResponsiveContainer width="100%" height={250}>
               <BarChart data={lastWeekWAUByCity}>
                 <CartesianGrid stroke="#ccc" strokeDasharray="3 3" vertical={false}/>
                 <XAxis dataKey="city"/>
                 <YAxis />
                 <Tooltip />
                 {/* <Legend verticalAlign="top" height={36}/> */}
-                <Bar name="上週各縣市活躍人數" dataKey="active_student_cnt" fill="#3399FF"/>
+                <Bar name="上週各縣市活躍人數" dataKey="active_student_cnt" fill={colors20[4]}/>
               </BarChart>
             </ResponsiveContainer>
           </Paper>
@@ -107,11 +147,52 @@ function App() {
       <div >&zwj;</div>
 
       <Typography variant="h5" paragraph={true}  color="secondary">
-        累計至當前數據 (2012-10~{WAUWeekByWeek[WAUWeekByWeek.length-1].week})
+        長期數據 (2012-10 ~ {WAUWeekByWeek[WAUWeekByWeek.length-1].week})
       </Typography>
 
       <Grid container spacing={4}>
         <Grid item xs>
+          <Typography variant="h6">
+            每週活躍使用者人數
+          </Typography>
+          <Paper className={classes.paper}>
+            <ResponsiveContainer width="100%" height={250}>
+              <LineChart data={WAUWeekByWeek}>
+                <Line name="週活躍人數" type="monotone" dataKey="unique_user_cnt" stroke={colors20[8]} dot={false}/>
+                <CartesianGrid stroke="#ccc" strokeDasharray="3 3" vertical={false}/>
+                <XAxis dataKey="week" tickFormatter={(tickItem) => tickItem.substring(0, 7)}/>
+                <YAxis />
+                <Tooltip />
+                {/* <Legend verticalAlign="top" height={36}/> */}
+              </LineChart>
+            </ResponsiveContainer>
+          </Paper>
+        </Grid>
+      </Grid>
+
+      <Grid container spacing={4}>
+        <Grid item xs>
+          <Typography variant="h6">
+            每月內容使用人次
+          </Typography>
+          <Paper className={classes.paper}>
+            <ResponsiveContainer width="100%" height={250}>
+              <LineChart data={contentUsageByMonth}>
+                <Line name="影片使用人次" type="monotone" dataKey="video_cnt" stroke={colors20[11]} dot={false}/>
+                <Line name="習題使用人次" type="monotone" dataKey="exercise_cnt" stroke={colors20[12]} dot={false}/>
+                <CartesianGrid stroke="#ccc" strokeDasharray="3 3" vertical={false}/>
+                <XAxis dataKey="year_month"/>
+                <YAxis />
+                <Tooltip />
+                {/* <Legend verticalAlign="top" height={36}/> */}
+              </LineChart>
+            </ResponsiveContainer>
+          </Paper>
+        </Grid>
+      </Grid>
+
+      <Grid container spacing={4}>
+        <Grid item xs={6}>
           <Typography variant="h6">
             各縣市不同身分註冊使用者統計
           </Typography>
@@ -121,7 +202,7 @@ function App() {
           </Paper>
         </Grid>
         
-        <Grid item xs>
+        <Grid item xs={6}>
           <Typography variant="h6">
             網站內容統計
           </Typography>
@@ -142,7 +223,7 @@ function App() {
             <Grid item xs>
               <Paper className={classes.paper}>
                 <Typography variant="h4" paragraph={true}  align="center">
-                {contentType[0].exercise.toLocaleString('en')}
+                  {contentType[0].quiz.toLocaleString('en')}
                 </Typography>
                 <Typography variant="h5" paragraph={true} align="center">
                   練習題總數
@@ -154,7 +235,7 @@ function App() {
             <Grid item xs>
               <Paper className={classes.paper}>
                 <Typography variant="h4" paragraph={true} align="center">
-                  19,581
+                  {contentType[0].video.toLocaleString('en')}
                 </Typography>
                 <Typography variant="h5" paragraph={true} align="center">
                   影片總數
@@ -169,14 +250,14 @@ function App() {
       <Grid container spacing={4}>
         <Grid item xs>
           <Typography variant="h6">
-            週活躍總人數
+            每月新增註冊人數
           </Typography>
           <Paper className={classes.paper}>
-            <ResponsiveContainer width="100%" height={300}>
-              <LineChart data={WAUWeekByWeek}>
-                <Line name="週活躍人數" type="monotone" dataKey="unique_user_cnt" stroke="#3399FF" dot={false}/>
+            <ResponsiveContainer width="100%" height={250}>
+              <LineChart data={regUserByMonth}>
+                <Line name="單月新增註冊人數" type="monotone" dataKey="monthly_register_count" stroke={colors20[14]} dot={false}/>
                 <CartesianGrid stroke="#ccc" strokeDasharray="3 3" vertical={false}/>
-                <XAxis dataKey="week" tickFormatter={(tickItem) => tickItem.substring(0, 7)}/>
+                <XAxis dataKey="date" tickFormatter={(tickItem) => tickItem.substring(0, 7)}/>
                 <YAxis />
                 <Tooltip />
                 {/* <Legend verticalAlign="top" height={36}/> */}
